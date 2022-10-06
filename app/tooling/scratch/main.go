5b4af442591eebf615b447b25e99fb10bf932216
William Kennedy <bill@ardanlabs.com>
2022-10-06 11:43:56 -0500

    push5

--- app/tooling/scratch/main.go
+++ app/tooling/scratch/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/ardanlabs/blockchain/foundation/blockchain/database"
+	"github.com/ardanlabs/blockchain/foundation/blockchain/signature"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
@@ -19,21 +18,46 @@ func main() {
 }
 
 func sendTx() error {
-	tx, err := database.NewTx(1, 1, "bill", "heather", 100, 0, nil)
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("unable to load private key for node: %w", err)
+	}
+
+	tx, err := database.NewTx(1, 1, "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32", "heather", 100, 0, nil)
 	if err != nil {
 		return fmt.Errorf("new tx: %w", err)
 	}
 
-	data, err := json.Marshal(tx)
+	signedTx, err := tx.Sign(privateKey)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("sign tx: %w", err)
 	}
 
-	resp, err := http.Post(fmt.Sprintf("%s/v1/tx/submit", "http://0.0.0.0:8080"), "application/json", bytes.NewBuffer(data))
+	// =========================================================================
+
+	tx2, err := database.NewTx(1, 1, "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32", "heather", 100, 0, nil)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("new tx2: %w", err)
 	}
-	defer resp.Body.Close()
+
+	addr, err := signature.FromAddress(tx2, signedTx.V, signedTx.R, signedTx.S)
+	if err != nil {
+		return fmt.Errorf("address: %w", err)
+	}
+
+	fmt.Println("*****>  0xF01813E4B85e178A83e29B8E7bF26BD830a25f32")
+	fmt.Println("*****> ", tx2.FromID == addr)
+
+	// data, err := json.Marshal(signedTx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// resp, err := http.Post(fmt.Sprintf("%s/v1/tx/submit", "http://0.0.0.0:8080"), "application/json", bytes.NewBuffer(data))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer resp.Body.Close()
 
 	return nil
 }
